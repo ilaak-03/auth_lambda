274ba2d6845fbd2dafb67e65c11678ed1cd7725b
ilaak-03 <Ilaa@meyicloud.com>
2025-12-18 11:58:08 +0530

    Update reset_password.py

--- reset_password.py
+++ reset_password.py
@@ -1,14 +1,19 @@
 import os
-from cognito_utils import cognito
+from cognito_utils import cognito, get_secret_hash
 
-def handle_reset_password(email, new_password):
-    cognito.admin_set_user_password(
-        UserPoolId=os.environ["COGNITO_USER_POOL_ID"],
-        Username=email,
-        Password=new_password,
-        Permanent=True
-    )
+def handle_reset_password(email, code, new_password):
+    try:
+        cognito.confirm_forgot_password(
+            ClientId=os.environ["COGNITO_CLIENT_ID"],
+            SecretHash=get_secret_hash(email),
+            Username=email,
+            ConfirmationCode=code,
+            Password=new_password
+        )
 
-    return {
-        "message": "Password reset successfully "
-    }
+        return {
+            "message": "Password reset successful"
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
