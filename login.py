080f8f2fc2f18f21289bfdc6dff7e15fbc081fb1
ilaak-03 <Ilaa@meyicloud.com>
2025-12-18 11:53:30 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,20 +1,32 @@
 import os
+from botocore.exceptions import ClientError
 from cognito_utils import cognito, get_secret_hash
 
 def handle_login(email, password):
-    resp = cognito.initiate_auth(
-        ClientId=os.environ["COGNITO_CLIENT_ID"],
-        AuthFlow="USER_PASSWORD_AUTH",
-        AuthParameters={
-            "USERNAME": email,
-            "PASSWORD": password,
-            "SECRET_HASH": get_secret_hash(email)
-        }
-    )
+    try:
+        resp = cognito.initiate_auth(
+            ClientId=os.environ["COGNITO_CLIENT_ID"],
+            AuthFlow="USER_PASSWORD_AUTH",
+            AuthParameters={
+                "USERNAME": email,
+                "PASSWORD": password,
+                "SECRET_HASH": get_secret_hash(email)
+            }
+        )
 
-    return {
-        "message": "Login successful",
-        "id_token": resp["AuthenticationResult"]["IdToken"],
-        "access_token": resp["AuthenticationResult"]["AccessToken"],
-        "refresh_token": resp["AuthenticationResult"]["RefreshToken"]
-    }
+        return {
+            "message": "Login successful",
+            "id_token": resp["AuthenticationResult"]["IdToken"],
+            "access_token": resp["AuthenticationResult"]["AccessToken"],
+            "refresh_token": resp["AuthenticationResult"]["RefreshToken"]
+        }
+
+    except ClientError as e:
+        # Handles:
+        # - UserNotConfirmedException
+        # - NotAuthorizedException
+        # - UserNotFoundException
+        return {"error": e.response["Error"]["Message"]}
+
+    except Exception as e:
+        return {"error": str(e)}
